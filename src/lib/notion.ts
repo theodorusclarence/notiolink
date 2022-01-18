46ba0c74afe5c7acbc3ff0cfdeb7dab44df11a13
Theodorus Clarence <theodorusclarence@gmail.com>
2022-01-18 12:06:23 +0700

    feat: add increment link count

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -8,6 +8,13 @@ const NOTION_INTEGRATION_SECRET =
 
 const notion = new Client({ auth: NOTION_INTEGRATION_SECRET });
 
+export type Url = {
+  pageId: string;
+  slug: string;
+  link?: string;
+  count: number;
+};
+
 /**
  * Get long URL by slug
  */
@@ -25,11 +32,35 @@ export const getUrlBySlug = async (slug: string) => {
   });
 
   const results = response.results[0] as unknown as LinkResult;
-  const url = {
-    slug: results?.properties.slug.title[0].plain_text,
-    link: results?.properties.link.rich_text[0].plain_text,
-    count: Number(results?.properties.count.rich_text[0].plain_text),
+
+  const url: Url = {
+    pageId: results?.id,
+    slug: results?.properties.slug.title[0]?.plain_text,
+    link: results?.properties.link.rich_text[0]?.plain_text,
+    count: Number(results?.properties.count.rich_text[0]?.plain_text ?? 0),
   };
 
   return url;
+};
+
+/**
+ * Increment count column by 1
+ */
+export const incrementLinkCount = async (url: Url) => {
+  if (!NOTION_LINK_DATABASE_ID) {
+    throw new Error('NEXT_PUBLIC_NOTION_LINK_DATABASE_ID env is not defined');
+  }
+
+  if (!url.pageId) {
+    throw new Error('URL data is not found');
+  }
+
+  await notion.pages.update({
+    page_id: url.pageId,
+    properties: {
+      count: {
+        rich_text: [{ text: { content: String(url.count + 1) } }],
+      },
+    },
+  });
 };
