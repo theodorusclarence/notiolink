1c703d54ef01a3231ffcc599f0a62b48d8091b15
Theodorus Clarence <theodorusclarence@gmail.com>
2022-01-18 12:47:47 +0700

    refactor: use api for increment

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import { getUrlBySlug, incrementLinkCount } from '@/lib/notion';
+import { getUrlBySlug } from '@/lib/notion';
 
 export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname.split('/')[1];
@@ -13,7 +13,15 @@ export default async function middleware(req: NextRequest) {
   const url = await getUrlBySlug(path);
 
   if (url.link) {
-    await incrementLinkCount(url);
+    // using fetch because edge function won't allow patch request
+    await fetch(req.nextUrl.origin + '/api/increment', {
+      method: 'POST',
+      body: JSON.stringify(url),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
     return NextResponse.redirect(url.link);
   }
 }
