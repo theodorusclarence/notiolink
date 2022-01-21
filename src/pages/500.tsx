a3db99f24d43bc491c518cea604d6651abdc0eec
Theodorus Clarence <theodorusclarence@gmail.com>
2022-01-21 11:20:23 +0700

    feat: add 404 and 500 page

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -4,19 +4,19 @@ import Accent from '@/components/Accent';
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
 
-export default function NotFoundPage() {
+export default function ErrorPage() {
   return (
     <Layout>
-      <Seo templateTitle='Link not found' />
+      <Seo templateTitle='Error' />
 
       <main>
         <section className=''>
           <div className='layout flex flex-col justify-center items-center py-20 min-h-screen'>
             <h1 className='text-5xl md:text-7xl'>
-              <Accent>Link not found</Accent>
+              <Accent>Error occurred</Accent>
             </h1>
             <p className='mt-4 text-gray-300'>
-              Are you sure it is the right link?
+              Sorry, there must be something wrong, please try again
             </p>
           </div>
         </section>
