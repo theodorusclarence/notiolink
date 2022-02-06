2cff127c749c0715de55828d205ff191b403d07e
Theodorus Clarence <theodorusclarence@gmail.com>
2022-02-06 13:02:04 +0700

    feat: add clarence.link config

--- notiolink.config.js
+++ notiolink.config.js
@@ -1,20 +1,20 @@
 module.exports = {
   /** Application Name, will be shown on the index page */
-  appName: 'Notiolink',
+  appName: 'clarence.link',
 
   /** Description for SEO */
   seoDescription:
     'Self-hostable branded link shortener built with Next.js & Notion API',
 
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  deployUrl: 'https://notiolink.thcl.dev',
+  deployUrl: 'https://clarence.link',
 
   /**
    * Color theme for the app
    * @type {'light' | 'dark' | 'milky' | 'street' | 'monokai'}
    */
-  theme: 'dark',
+  theme: 'monokai',
 
   /** REQUIRED CONFIG: Set this to 'false'  */
-  demoMode: 'true',
+  demoMode: 'false',
 };
