b62728b42e7972b01ca0e8c2ddfee3558cb14295
Theodorus Clarence <theodorusclarence@gmail.com>
2022-03-01 14:27:09 +0700

    feat: add new link on 404 for admin

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,20 @@
+import { useRouter } from 'next/router';
 import * as React from 'react';
+
+import { getFromLocalStorage } from '@/lib/helper';
 
 import Accent from '@/components/Accent';
 import Layout from '@/components/layout/Layout';
+import ArrowLink from '@/components/links/ArrowLink';
+import PrimaryLink from '@/components/links/PrimaryLink';
 import Seo from '@/components/Seo';
 
 export default function NotFoundPage() {
+  const router = useRouter();
+  const firstPath = router.asPath.split('/')[1];
+
+  const token = getFromLocalStorage('@notiolink/app_token');
+
   return (
     <Layout>
       <Seo templateTitle='Link not found' />
@@ -18,6 +28,16 @@ export default function NotFoundPage() {
             <p className='mt-4 text-gray-300'>
               Are you sure it is the right link?
             </p>
+
+            {token && (
+              <ArrowLink
+                href={`/new?slug=${firstPath}`}
+                className='mt-8'
+                as={PrimaryLink}
+              >
+                <Accent>Shorten link to /{firstPath}</Accent>
+              </ArrowLink>
+            )}
           </div>
         </section>
       </main>
